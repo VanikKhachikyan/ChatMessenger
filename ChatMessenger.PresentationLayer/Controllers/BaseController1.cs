c710dc9a78b4b01fbae17519f670c31e931afa70
Van <van_khachikyan@outlook.com>
2019-01-29 23:50:00 +0400

    ____________

---
+++ ChatMessenger.PresentationLayer/Controllers/BaseController1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatMessenger.BusinessLogicLayer.Factories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ChatMessenger.PresentationLayer.Controllers
+{
+    public class BaseController1 : Controller
+    {
+        protected readonly AbstractFactory _blFactory;
+    }
+}
